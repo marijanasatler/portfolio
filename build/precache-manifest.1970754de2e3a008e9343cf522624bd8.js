c507d7dfa07310b7a807771efbce021a4a77a99a
Marijana Satler <marijanasatler@gmail.com>
2020-06-25 13:49:50 +0200

    gh pages

---
+++ build/precache-manifest.1970754de2e3a008e9343cf522624bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4552375621efe1b1cea13de330e0690",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6639a307055940c6f26d",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/portfolio/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6639a307055940c6f26d",
+    "url": "/portfolio/static/js/main.cb50294d.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  }
+]);
