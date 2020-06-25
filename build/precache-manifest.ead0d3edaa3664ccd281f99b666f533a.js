ce8615c123accbfb71de67fdf0f6c36db3c9b560
Marijana Satler <marijanasatler@gmail.com>
2020-06-25 14:16:50 +0200

    check

---
+++ build/precache-manifest.ead0d3edaa3664ccd281f99b666f533a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db858e598f8a9138782c2a98a0168723",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0d7feccd9946cb4714e2",
+    "url": "/portfolio/static/css/main.589f9fa1.chunk.css"
+  },
+  {
+    "revision": "ca9a30fa32a88ad73195",
+    "url": "/portfolio/static/js/2.f0e95a0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.f0e95a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7feccd9946cb4714e2",
+    "url": "/portfolio/static/js/main.740dce9f.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "53c42f9664ec8da5bcd5899961867609",
+    "url": "/portfolio/static/media/logo.53c42f96.png"
+  },
+  {
+    "revision": "2f1cb5b1f998f9ca6816fed8ff00244e",
+    "url": "/portfolio/static/media/logo1.2f1cb5b1.png"
+  },
+  {
+    "revision": "791725fd44f19859f0cbc1be240ef44d",
+    "url": "/portfolio/static/media/logo3.791725fd.png"
+  },
+  {
+    "revision": "47f84f2f097553297e74e522b11d8485",
+    "url": "/portfolio/static/media/logo4.47f84f2f.png"
+  },
+  {
+    "revision": "762c80dd4230603f2dcad0cecde051f5",
+    "url": "/portfolio/static/media/mylogo.762c80dd.png"
+  },
+  {
+    "revision": "d617ee341c02e053c1f2bcfc0eb96bda",
+    "url": "/portfolio/static/media/mypic.d617ee34.jpg"
+  },
+  {
+    "revision": "08857db1dae4f18c13b0ad922a2c6f91",
+    "url": "/portfolio/static/media/wds052801.08857db1.woff"
+  }
+]);
